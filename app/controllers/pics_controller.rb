55f258a3cdfb562e9d5b41aa4b83288719209b4d
manhmeoday <Nguyen Duc Manh Mmo>
2018-04-13 18:33:40 +0700

    make paper clip

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,49 @@
+class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+  def index
+  @pics = Pic.all.order("created_at DESC")
+  end
+  
+  def show
+  @pic = Pic.find(params[:id])
+  end
+  
+  def new
+  @pic= current_user.pics.build
+  end 
+  
+  def create
+  @pic= current_user.pics.build(pic_params)
+  
+   if @pic.save
+     redirect_to @pic, notice: "Sucessfully posted"
+   else
+     render 'new'
+   end
+   
+   def edit
+   end
+   
+   def update 
+     if @pic.update(pic_params)
+       redirect_to @pic, notice: "Updated sucsessfully"
+     else
+       render 'edit'
+     end
+   end
+   
+   def destroy
+   @pic.destroy
+   redirect_to root_path
+   end
+  end
+  
+  private
+  def pic_params
+    params.require(:pic).permit(:title, :description)
+  end
+  
+  def find_pic
+  @pic = Pic.find(params[:id])
+  end
+end
